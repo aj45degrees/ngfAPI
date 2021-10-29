01fbdb3f838f0d4df916771cc862ad4ac7583077
ajigherighe <aj@redstonema.com>
2021-10-29 16:57:27 -0400

    created initial calc and tests files

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
 FROM python:3.10.0-alpine
 LABEL authors="45 Degrees && Caffeine Devs"
 
+RUN pip install --upgrade pip
+
 ENV PYTHONBUFFERED 1
 
 COPY ./requirements.txt /requirements.txt
--- app/app/calc.py
+++ app/app/calc.py
@@ -1,3 +1,9 @@
-def add (x: int, y: int)-> int:
+
+def add(x: int, y: int) -> int:
     """Adds two integers and returns the integer sum"""
     return x + y
+
+
+def subtract(x: int, y: int) -> int:
+    """Subtract first integer from second and returns the integer result"""
+    return y - x
